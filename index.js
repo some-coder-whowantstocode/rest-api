30b549d4ada458d82414b308c99fe775282e75ba
Rohit <rohit19003@gmail.com>
2023-06-06 18:51:20 +0530

    some changes

--- index.js
+++ index.js
@@ -18,17 +18,13 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-app.use(helmet)
-app.use(cors)
-app.use(xss)
-app.use(rate)
-// extra packages
+
 
 // routes
 
-// app.get('/',(req,res)=>{
-//   res.send('hi');
-// })
+app.get('/',(req,res)=>{
+  res.send('hi');
+})
 
 app.use('/api/v1/auth',require('./routes/auth'));
 app.use('/api/v1/job',authenticate,require('./routes/jobs'));
